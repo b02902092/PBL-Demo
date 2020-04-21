a45995c41d46a04c1c55bd1af546a665b7b96cd4
b02902092 <b02902092@ntu.edu.tw>
2020-04-21 11:33:34 +0900

    slack income webhook can work

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    public static void main(String[] args) {
+        PostJson p = new PostJson();
+        String path = "https://hooks.slack.com/services/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";
+        String json = "{\"text\":\"Test Message\"}";
+/*
+        String path = "https://slack.com/api/chat.postMessage";
+        String token = "xoxb-2451879749-1066005771747-mXYRM6xNKqJUXizAieKqAA79";
+        String user = "lai.ting.wei";
+        String json = "{" +
+                "\"token\"=\"" + token + "\"," +
+                "\"channel\"=\"@" + user + "\"," +
+                "\"text\"=\"" + "Hello World!" + "\"," +
+                "\"as_user\"=\"1\"" +
+                "}";*/
 
-	public static void main(String[] args) throws Throwable {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+        System.out.println(json + "\n" + path);
+        System.out.println(p.postJson(json, path));
+    }
 }
