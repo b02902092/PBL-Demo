44213db90c9703b662f5291819caab3ac0a749a0
b02902092 <b02902092@ntu.edu.tw>
2020-05-08 17:05:59 +0900

    remove test code

--- LoginPage/src/main/java/com/example/demo/PblController.java
+++ LoginPage/src/main/java/com/example/demo/PblController.java
@@ -6,18 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PblController {
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello World\n";
-    }
-
-    @GetMapping("/users/{username}")
-    @ResponseBody
-    public String useProfile (@PathVariable String username) {
-        return String.format("user: %s", username);
-    }
-
     @RequestMapping(value="/index", method= RequestMethod.GET)
     public String sayHelloForm(Model model) {
         model.addAttribute("userProfile", new UserProfile());
