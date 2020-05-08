08254732f055252ffd4ee0719bc2f625cc8634aa
b02902092 <b02902092@ntu.edu.tw>
2020-05-08 17:06:23 +0900

    add max result

--- LoginPage/src/main/java/com/example/demo/UserProfile.java
+++ LoginPage/src/main/java/com/example/demo/UserProfile.java
@@ -4,10 +4,12 @@ public class UserProfile {
 
     private String name;
     private String country;
+    private int maxResult;
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -18,5 +20,13 @@ public class UserProfile {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public int getMaxResult() {
+        return maxResult;
+    }
+
+    public void setMaxResult(int maxResult) {
+        this.maxResult = maxResult;
     }
 }
